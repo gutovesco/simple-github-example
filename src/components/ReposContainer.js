b2f6be8b47a8758b28b1834b1cd4888a7a992623
gutovesco <guto.vesco@hotmail.com>
2020-02-04 20:36:50 -0300

    added input

--- src/components/ReposContainer.js
+++ src/components/ReposContainer.js
@@ -1,26 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import { fetchRepos } from './services/repos-api'
 
-
 export default function ReposContainer (){
     const [repos,setRepos] = useState({
-        repos: []
+        repos: [],
+        username: ''
     });
 
     useEffect(() => {
         fetchRepos('gutovesco').then(res => {
-            setRepos(res.data)
+            setRepos({repos: res.data})
             console.log(res.data)
         })
     }, [])
 
+    const handleChange = (e) => {
+        let username = e.target.value;
+
+        setRepos(prevState => {
+          return { ...prevState, username: username }
+        });
+      }
+
     return (
         <>
+        <h1>Repos</h1>
+        <form action="#">
+            <input 
+            onChange={handleChange}
+            placeholder="Informe o nome de usuario no github" 
+            type="search"
+            style={{width: '250px'}}
+            ></input>
+        </form>
         <ul>
-            {() => setRepos.map(repo => (
+            {repos.repos.map(repo => (
                  <li key={repo.id}>{repo.name}</li>
             ) )}
         </ul>
+    )
+}
         </>
     )
 }
